f95c4db88ef26c41ddba55e2b3c82b7eeb9ac8d2
fernandoaneto2 <fernando.neto02@gmail.com>
2025-03-18 22:08:46 -0300

    adicionando projeto para deploy

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
           <h1>RESTAURANT</h1>
           <p>
             De pratos clássicos a criações surpreendentes, nosso cardápio é um
-            requinte de sabores refinados.
+            requinte de sabores refinados...
           </p>
         </div>
       </header>
